7096535ce7970a9d1a0c6f8a69844b05f5be2ff3
Kibaara Mutethia <kibaara.aivyn@students.jkuat.ac.ke>
2026-06-24 17:38:11 +0300

    using expressjs router method for a cleaner code

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -3,28 +3,38 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>{
+const router = express.Router();
+
+let cars = [
+    {id: 1, make: 'Toyota', model: 'Camry', year: 2022, price: 28000},
+    {id: 2, make: 'Tesla', model: 'Model S', year: 2023, price: 25000},
+    {id: 3, make: 'Ford', model: 'F-150', year: 2021, price: 35000},
+]
+
+router.get('/', (req, res) =>{
     res.send('Hello from Cars API!');
 });
 
-app.get('/api/v1/cars', (req, res) =>{
+router.get('/', (req, res) =>{
     res.send('All Cars')
 });
 
-app.post('/api/v1/cars', (req, res) =>{
+router.post('/', (req, res) =>{
     res.send('New car');
 });
 
-app.post('/api/v1/cars/:id', (req, res) => {
+router.post('/:id', (req, res) => {
 res.send('Update car');
 });
 
-app.delete('api/v1/cars/:id', (red, res)=>{
+router.delete('/:id', (red, res)=>{
     res.send('Delete car');
 });
 
-app.get('/api/v1/cars/:id', (req, res) =>{
+router.get('/:id', (req, res) =>{
     res.send('Get car');
 })
+
+app.use('appi/v1', router)
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
